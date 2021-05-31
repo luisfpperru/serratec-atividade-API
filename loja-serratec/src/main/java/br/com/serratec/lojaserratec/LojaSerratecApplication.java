0c04313edea5457b13011423e664c776f778f7ee
luisfpperru <lfpperru@hotmail.com>
2021-05-31 08:45:13 -0300

    Primeiro commit Teste

---
+++ loja-serratec/src/main/java/br/com/serratec/lojaserratec/LojaSerratecApplication.java
@@ -0,0 +1,13 @@
+package br.com.serratec.lojaserratec;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LojaSerratecApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LojaSerratecApplication.class, args);
+	}
+
+}
---
+++ loja-serratec/src/test/java/br/com/serratec/lojaserratec/LojaSerratecApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.serratec.lojaserratec;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LojaSerratecApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
