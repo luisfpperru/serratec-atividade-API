ecae361601bdd303828ead05b5d3bc4b0368ea5c
luanaponte92 <81425309+luanaponte92@users.noreply.github.com>
2021-05-31 09:55:28 -0300

    criando as classes

--- loja-serratec/src/main/java/br/com/serratec/lojaserratec/model/Cliente.java
+++ loja-serratec/src/main/java/br/com/serratec/lojaserratec/model/Cliente.java
@@ -1,5 +1,55 @@
 package br.com.serratec.lojaserratec.model;
 
 public class Cliente {
+	     private Long id;
+	     private String nome;
+	     private Integer idade;
+	     private String sexo;
+	     private String estadoCivil;
+	     private String profissao;
+	     private String cpfOuCnpj;
+		public Long getId() {
+			return id;
+		}
+		public void setId(Long id) {
+			this.id = id;
+		}
+		public String getNome() {
+			return nome;
+		}
+		public void setNome(String nome) {
+			this.nome = nome;
+		}
+		public Integer getIdade() {
+			return idade;
+		}
+		public void setIdade(Integer idade) {
+			this.idade = idade;
+		}
+		public String getSexo() {
+			return sexo;
+		}
+		public void setSexo(String sexo) {
+			this.sexo = sexo;
+		}
+		public String getEstadoCivil() {
+			return estadoCivil;
+		}
+		public void setEstadoCivil(String estadoCivil) {
+			this.estadoCivil = estadoCivil;
+		}
+		public String getProfissao() {
+			return profissao;
+		}
+		public void setProfissao(String profissao) {
+			this.profissao = profissao;
+		}
+		public String getCpfOuCnpj() {
+			return cpfOuCnpj;
+		}
+		public void setCpfOuCnpj(String cpfOuCnpj) {
+			this.cpfOuCnpj = cpfOuCnpj;
+		}
+	     
+	}
 
-}
--- loja-serratec/src/main/java/br/com/serratec/lojaserratec/model/Produto.java
+++ loja-serratec/src/main/java/br/com/serratec/lojaserratec/model/Produto.java
@@ -2,4 +2,43 @@ package br.com.serratec.lojaserratec.model;
 
 public class Produto {
 
-}
+	    private Long id;
+	    private String nome;
+	    private Integer quantidade;
+	    private Double valor;
+	    private String observacao;
+		public Long getId() {
+			return id;
+		}
+		public void setId(Long id) {
+			this.id = id;
+		}
+		public String getNome() {
+			return nome;
+		}
+		public void setNome(String nome) {
+			this.nome = nome;
+		}
+		public Integer getQuantidade() {
+			return quantidade;
+		}
+		public void setQuantidade(Integer quantidade) {
+			this.quantidade = quantidade;
+		}
+		public Double getValor() {
+			return valor;
+		}
+		public void setValor(Double valor) {
+			this.valor = valor;
+		}
+		public String getObservacao() {
+			return observacao;
+		}
+		public void setObservacao(String observacao) {
+			this.observacao = observacao;
+		}
+	    
+
+	}
+
+
