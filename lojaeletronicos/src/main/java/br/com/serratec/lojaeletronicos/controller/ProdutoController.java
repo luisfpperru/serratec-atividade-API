70035414f040fb6cb05bbbd60ffebc8aff584476
zsazsaspeck <giovannemenezeslopes@hotmail.com>
2021-06-05 16:22:23 -0300

    Ajuda no controller

--- lojaeletronicos/src/main/java/br/com/serratec/lojaeletronicos/controller/ProdutoController.java
+++ lojaeletronicos/src/main/java/br/com/serratec/lojaeletronicos/controller/ProdutoController.java
@@ -1,5 +1,50 @@
 package br.com.serratec.lojaeletronicos.controller;
 
-public class ProdutoController {
+import java.util.List;
+import java.util.Optional;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import br.com.serratec.lojaeletronicos.model.Produto;
+import br.com.serratec.lojaeletronicos.repository.ProdutoRepository;
+
+    public class ProdutoController {
+	@Autowired
+	ProdutoRepository _repositorioProduto;
+	
+	@GetMapping
+	public List<Produto> obterTodos(){
+		return this._repositorioProduto.findAll();
+	}
+
+	@GetMapping("/{id}")
+	public Optional<Produto> obterPorId(@PathVariable(value = "id") Long id){
+		return this._repositorioProduto.findById(id);
+	}
+	
+	@PostMapping
+	public ResponseEntity<Produto> adicionar(@RequestBody Produto produto){
+		var adicionado = this._repositorioProduto.save(produto);
+        return new ResponseEntity<>(adicionado, HttpStatus.CREATED);
+	}
+	
+	 public Produto atualizar(@PathVariable(value = "id") Long id, @RequestBody Produto produto) {
+ 		_repositorioProduto.findById(id);
+ 						 //.orElseThrow( ()-> new NotFoundException("Categoria não encontrada pelo ID:" + id));
+ 		 produto.setId(id);
+         return this._repositorioProduto.save(produto);
+	 }
+
+	 @DeleteMapping("/id/{id}")
+	 public void deletar(@PathVariable(value = "id") Long id) {
+			_repositorioProduto.findById(id);
+							 //.orElseThrow( ()-> new NotFoundException("Categoria não encontrada pelo ID:" + id));
+         this._repositorioProduto.deleteById(id);
+	 }
 }
